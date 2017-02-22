7e0b4e95972ce916ca63edccf84a5fc7da883b06
Khrolenko <artem.khrolenko@gmail.com>
2017-02-23 00:32:22 +0200

    Task_02

---
+++ src/com/Week_02/Task02.java
@@ -0,0 +1,27 @@
+package com.Week_02;
+
+/**
+ * Використовуючи цикл for виведіть на екран матрицю. Зверніть увагу на форматування (відступи):
+
+ *  2  3  4  *
+ 6  *  8  * 10
+ 11 12  * 14 15
+ 16  * 18  * 20
+ * 22 23 24  *
+
+ */
+public class Task02 {
+    public static void main(String[] args) {
+        int k = 1;
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                if (i == j || j == 4 - i)
+                    System.out.print(" * ");
+                else
+                System.out.printf("%2d ", k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
