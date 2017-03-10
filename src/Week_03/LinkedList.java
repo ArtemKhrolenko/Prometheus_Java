981aa9372f7723e83a3c5dd43eec146853c7b24f
Khrolenko <Artem.Khrolenko@gmail.com>
2017-03-10 20:27:52 +0200

    Commit_LinkedList

--- src/Week_03/LinkedList.java
+++ src/Week_03/LinkedList.java
@@ -5,31 +5,84 @@ package Week_03;
  */
 public class LinkedList {
 
+    private Node nodeFirst;
+    private Node nodeLast;
+    private int count;
+
+
     /* Конструктор без аргументів */
     public LinkedList() {
+
 
     }
 
     /* Додати елемент в кінець списку */
     public void add(Integer data) {
+        Node elem = new Node();
+        elem.setData(data);
+        if (nodeFirst == null){
+            nodeFirst = elem;
+            nodeLast = elem;
+        }
+        else {
+            nodeLast.setNext(elem);
+            nodeLast = elem;
+        }
+        this.count++;
 
     }
 
     /* Отримати елемент по індексу, повертає null якщо такий елемент недоступний */
     public Integer get(int index) {
-        return 0;
+
+        Node getElem = nodeFirst;
+        if (index >= this.count || index < 0) return null;
+        else{
+            for (int i = 0; i < index; i++){
+                getElem = getElem.getNext();
+            }
+        }
+        return getElem.getData();
     }
 
     /* Вилучення елементу за індексом, повертає true у разі успіху або false в іншому випадку */
     public boolean delete(int index) {
+        Node prev = null;
+        Node current = nodeFirst;
+
+        if (index < this.count && index >= 0){
+            for (int i = 0; i < index; i++) {
+                prev = current;
+                current = current.getNext();
+            }
+
+            // Узел в середине или в конце.
+            if (prev != null) {
+                prev.setNext(current.getNext());
+
+                // Если в конце, то меняем _tail.
+                if (current.getNext() == null) {
+                    nodeLast = prev;
+                }
+            }
+            else //в начале списка
+            {
+                nodeFirst = nodeFirst.getNext();
+
+                // Список теперь пустой?
+                if (nodeFirst == null){
+                    nodeLast = null;
+                }
+            }
+            this.count--;
+            return true;
+        }
         return false;
     }
 
     /*Поверта розмір списку: якщо елементів в списку нема то повертає 0 (нуль)*/
     public int size() {
-        return 0;
+        return this.count;
     }
-
-
 
 }
---
+++ src/Week_03/ListTest.java
@@ -0,0 +1,31 @@
+package Week_03;
+
+/**
+ * Created by Khrolenko on 10.03.2017.
+ */
+public class ListTest {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(5);
+        list.add(8);
+        list.add(4);
+        list.add(6);
+
+        System.out.println("size = " + list.size());
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        System.out.println(list.delete(0));
+
+        System.out.println("size = " + list.size());
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+    }
+}
+
+
