dbaf1d5daafda2bba1689991badc17d5f32f7ec3
Khrolenko <Artem.Khrolenko@gmail.com>
2017-03-10 18:29:13 +0200

    Commit_Work

---
+++ src/Week_03/LinkedList.java
@@ -0,0 +1,35 @@
+package Week_03;
+
+/**
+ * Created by Khrolenko on 10.03.2017.
+ */
+public class LinkedList {
+
+    /* Конструктор без аргументів */
+    public LinkedList() {
+
+    }
+
+    /* Додати елемент в кінець списку */
+    public void add(Integer data) {
+
+    }
+
+    /* Отримати елемент по індексу, повертає null якщо такий елемент недоступний */
+    public Integer get(int index) {
+        return 0;
+    }
+
+    /* Вилучення елементу за індексом, повертає true у разі успіху або false в іншому випадку */
+    public boolean delete(int index) {
+        return false;
+    }
+
+    /*Поверта розмір списку: якщо елементів в списку нема то повертає 0 (нуль)*/
+    public int size() {
+        return 0;
+    }
+
+
+
+}
---
+++ src/Week_03/Node.java
@@ -0,0 +1,25 @@
+package Week_03;
+
+/**
+ * Created by Khrolenko on 10.03.2017.
+ */
+public class Node {
+    private Node next;
+    private Integer data;
+
+    public Node() {
+    }
+
+    public Node getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    public Integer getData() {
+        return data;
+    }
+    public void setData(Integer data) {
+        this.data = data;
+    }
+}
